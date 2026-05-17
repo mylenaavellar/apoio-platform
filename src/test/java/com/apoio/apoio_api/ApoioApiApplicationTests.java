1721fe26baf25dbe17c0e418fcf737b4389ecd6b
Mylena Avellar <mylenatavellar@gmail.com>
2026-05-17 11:30:38 -0300

    chore: configure initial project environment, maven dependencies, and wsl postgres connection

---
+++ src/test/java/com/apoio/apoio_api/ApoioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apoio.apoio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
